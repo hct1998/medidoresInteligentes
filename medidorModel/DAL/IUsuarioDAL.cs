c21710c2a9ad35714d398a6229b96d625f9966f0
HECTOR JAVIER LECAROS GONZALEZ <hector.lecaros@inacapmail.cl>
2022-07-14 23:54:40 -0400

    1

---
+++ medidorModel/DAL/IUsuarioDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medidorModel.DAL
+{
+    public interface IUsuarioDAL
+    {
+        List<Usuario> ObtenerUsuarios();
+
+
+    }
+}
---
+++ medidorModel/DAL/MedidorDALDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medidorModel.DAL
+{
+    class MedidorDALDB
+    {
+        private medidorappEntities medidorDB = new medidorappEntities();
+
+        public void actualizarMedidor(Medidor m)
+        {
+            //Medidor mOriginal = this.medidorDB.usuarios.Find(m.id_medidor);
+        }
+
+
+
+        public void AgregarMedidor(Medidor medidor)
+        {
+            this.medidorDB.Medidors.Add(medidor);
+            this.medidorDB.SaveChanges();
+
+        }
+
+
+        
+    }
+}
